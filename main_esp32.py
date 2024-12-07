3eef4df08a426d3bbcb78657cfc88a07eb8ec665
George Cristian Raducanu <92454682+GeorgeRaducanu@users.noreply.github.com>
2024-12-07 10:42:06 +0200

    Modification on board code

--- main_esp32.py
+++ main_esp32.py
@@ -1,10 +1,21 @@
 import dht
 import machine
 import time
-
+import network
+import urequests
+import ussl
 DHT_PIN = 4  # Pinul pentru senzor
 
 dht_sensor = dht.DHT11(machine.Pin(DHT_PIN))
+
+ssid = "Numele retelei"
+password = "Parola retelei"
+url = "https://192.168.1.5:5001/"
+
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect(ssid, password)
+
 
 while True:
     try:
